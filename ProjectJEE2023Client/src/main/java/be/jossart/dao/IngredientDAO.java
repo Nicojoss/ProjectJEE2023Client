d5f2df682cd250f657eb72525c8dadc9e750423c
nicojoss <nico.jossart@condorcet.be>
2023-12-15 18:57:13 +0100

    Initial Commit

---
+++ ProjectJEE2023Client/src/main/java/be/jossart/dao/IngredientDAO.java
@@ -0,0 +1,39 @@
+package be.jossart.dao;
+
+import java.util.ArrayList;
+
+import be.jossart.javabeans.Ingredient;
+
+public class IngredientDAO extends DAO<Ingredient>{
+	
+	@Override
+	public boolean create(Ingredient obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Ingredient obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Ingredient obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Ingredient find(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Ingredient> findAll(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
